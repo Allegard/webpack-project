5de9dc150806f6e510eceeb630278a23e6b8bce4
Morales Alfaro <anof@gft.com>
2021-08-01 20:29:31 -0600

    new files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles/index.scss";
+
+const info = {
+    name: "Adrian",
+    age: 23,
+    country: "CR"
+};
+
+const info2 = {
+    name: "Adrian",
+    lastname: "Morales",
+    age: 23,
+    country: "CR"
+};
+
+console.log(info);
+console.log(info2);
